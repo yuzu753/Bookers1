88d41f4ab2c3e266b15cae15eaafb703497c86c8
yuzu753 <nakago0513@gmail.com>
2019-10-14 07:13:31 +0000

    bookers1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+      @books = Book.all
+      @book = Book.new
+  end
+
+  def new
+  	  @book = Book.new
+  end
+
+  def create
+  	  @book = Book.new(book_params)
+  	  if @book.save
+         flash[:notice] = "Book was successfully created."
+         redirect_to book_path(@book.id)
+      else
+         @books = Book.all
+         render action: :index
+      end
+  end
+
+  def edit
+  	  @book = Book.find(params[:id])
+  end
+
+  def update
+  	  @book = Book.find(params[:id])
+  	  if @book.update(book_params)
+         flash[:notice] = "Book was successfully created."
+  	     redirect_to book_path(@book.id)
+      else
+         flash[:notice] = "Error"
+         redirect_to edit_book_path(@book.id)
+      end
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def destroy
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
+  end
+
+  private
+  def book_params
+  	 params.require(:book).permit(:title, :body)
+  end
+
+end
+
